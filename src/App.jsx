c19b4d23e74a401e4573368dfb915b4aaf7c4908
kennzuki <kingsyken@gmail.com>
2024-11-13 16:34:54 +0300

    todos fetch with error and isloading

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 function App() {
-  const {data} = useQuery({
+  const {data,error,isLoading} = useQuery({
     queryKey: ['todo'],
     queryFn: () =>
       fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
@@ -10,14 +10,19 @@ function App() {
       ),
   });
 
-  return <div className="p-12 border text-black ">
-    {data.map((todo) => (
-      <div key={todo.id} className="">
+  if (error) <div className="text-red-500">There was an error loading</div>
+  if (isLoading) <div className="text-yellow-500">Data is loading...</div>
+
+  return <div className="p-12 border ">
+   
+   {data.map((todo) => (
+      <div key={todo.id} className="flex flex-col gap-4 border p-4">
         <h1 className="">{todo.id}</h1>
         <h1 className="">{todo.title}</h1>
         <h1 className="">{todo.completed}</h1>
       </div>
     ))}
+   
   </div>;
 }
 
